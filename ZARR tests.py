155330e67a6b14e5eae926c2aa5d4f2e58307786
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2022-10-24 01:25:58 +0200

    Updated ZARR test.py

--- ZARR tests.py
+++ ZARR tests.py
@@ -5,33 +5,22 @@ import time
 from numcodecs import Blosc
 import dask.array as da
 import s3fs
-# %% FUNCTIONS
-# %% TESTS
-
-# create a NumPy dummy array:
+# %% CREATE A DUMMY NUMPY ARRAY
 np.random.seed(1)
 array_2D = np.random.rand(1000,1000)
 array_3D = np.random.rand(1000,1000,100)
 
+# save the array as textfiles and as a NumPy file:
+np.save("array_2D.npy", array_2D)
 np.savetxt("array_2D.txt", array_2D, delimiter=" ")
-
-# np.savetxt("array_3D.txt", array_3D, delimiter=" ")
-"""this will not work: ValueError: Expected 1D or 2D array, got 3D array instead """
-
 np.save("array_3D.npy", array_3D)
-
-zarr.save('array_3D.zarr', array_3D)
-
-array_3D_zarr = zarr.load('array_3D.zarr')
-print(type(array_3D_zarr))
-
+# np.savetxt("array_3D.txt", array_3D, delimiter=" ")
+"""the latter will not work: ValueError: Expected 1D or 2D array, got 3D array instead """
 # %% ZARR CONVENIENT SAVE AND LOAD
 """convenient save and load (save right away and load directly into memory:"""
 zarr_out_3D_convenient = zarr.save('zarr_out_3D_convenient.zarr', array_3D)
 zarr_in_3D_convenient  = zarr.load('zarr_out_3D_convenient.zarr')
 print(type(zarr_in_3D_convenient))
-
-
 # %% ZARR WITH MORE CONTROLS
 """save with more controls and do not load directly into memory:"""
 zarr_out_3D = zarr.open('zarr_out_3D.zarr', mode='w', shape=array_3D.shape,
@@ -58,21 +47,19 @@ print(type(zarr_out_root["array_2D"]))
 
 print(zarr_out_root["array_3D"].info)
 print(zarr_out_root["array_2D"].info)
-
-# %% ZARR AND MEMORY MAPPING
+# %% NUMPY MEMORY MAPPING
 numpy_mmap_3D = np.memmap("numpy_out_3D.arr", mode="w+", dtype=array_3D.dtype, shape=array_3D.shape)
 numpy_mmap_3D[:] = array_3D
 numpy_mmap_3D.flush()
 
 numpy_mmap_3D_read = np.memmap("numpy_out_3D.arr", mode="r", dtype=array_3D.dtype, shape=array_3D.shape)
 print(np.array_equal(array_3D, numpy_mmap_3D_read))
-# %% ZAR COMPRESSION
+# %% ZARR COMPRESSION
 compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
 zarr_out_3D = zarr.open('zarr_out_3D_compressed.zarr', mode='w', shape=array_3D.shape,
                         chunks=(1000,1000,1), dtype=array_3D.dtype, compressor=compressor)
 zarr_out_3D[:] = array_3D
 print(zarr_out_3D.info)
-
 
 array_3D_int = np.random.randint(low=1, high=10, size=(1000,1000,100), dtype="int64")
 compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
@@ -103,15 +90,15 @@ zarr_out_root.attrs["date"]   = "Oct 13, 2022"
 zarr_out_root.attrs["description"]  = "A Zarr file containing some NumPy arrays"
 
 # ...to groups:
-zarr_out_root["sub_arrays"].attrs["description"] = "A test sub-group"
+zarr_out_root["group_1"].attrs["description"] = "A test sub-group"
 
 # ...and to arrays:
-zarr_out_root["sub_arrays/array_1D_1"].attrs["description"] = "A test array"
+zarr_out_root["group_1/array_1D_1"].attrs["description"] = "A test array"
 
 for key in zarr_out_root.attrs.keys():
     print(f"{key}: {zarr_out_root.attrs[key]}")
-print(zarr_out_root["sub_arrays"].attrs["description"])
-print(zarr_out_root["sub_arrays/array_1D_1"].attrs["description"])
+print(zarr_out_root["group_1"].attrs["description"])
+print(zarr_out_root["group_1/array_1D_1"].attrs["description"])
 # %% DISTRIBUTED STORAGE
 s3_fs     = s3fs.S3FileSystem(anon=True, client_kwargs=dict(region_name='eu-west-2'))
 s3_store  = s3fs.S3Map(root='zarr-demo/store', s3=s3_fs, check=False)
@@ -120,38 +107,4 @@ z_array   = zarr_root['foo/bar/baz']
 print(z_array)
 print(z_array.info)
 print(z_array[:].tobytes())
-# %% DASK
-import dask.array as da
-
-
-# %% ZARR ADVANCED
-zarrstore = zarr.DirectoryStore("zarr_array.zarr")
-zarr_ds   = zarr.group(store=zarrstore, overwrite=True)
-zarr_out_3D = zarr_ds.create_dataset("array_3D", data=array_3D)
-zarr_out_2D = zarr_ds.create_dataset("array_2D", data=array_2D)
-
-zarr_out_3D.attrs["shape"]  = array_3D.shape
-zarr_out_3D.attrs["author"] = "Pixel Tracker"
-zarr_out_3D.attrs["date"]   = "Oct 13, 2022"
-
-for key in zarr_out_3D.attrs.keys():
-    print(f"{key}: {zarr_out_3D.attrs[key]}")
-
-compressor=Blosc(cname='zstd', clevel=0, shuffle=Blosc.SHUFFLE)
-zarr_out = zarr_ds.create_dataset("array_3D_compress_level_0", data=array_3D, compressor=compressor,overwrite=True)
-
-compressor=Blosc(cname='zstd', clevel=1, shuffle=Blosc.SHUFFLE)
-zarr_out = zarr_ds.create_dataset("array_3D_compress_level_1", data=array_3D, compressor=compressor,overwrite=True)
-
-compressor=Blosc(cname='zstd', clevel=5, shuffle=Blosc.SHUFFLE)
-zarr_out = zarr_ds.create_dataset("array_3D_compress_level_5", data=array_3D, compressor=compressor,overwrite=True)
-
-dump = zarr.open("zarr_array.zarr")
-print(type(dump["array_3D"]))
-print(type(dump["array_3D"][:]))
-
-dask_zarr_array = da.from_zarr("zarr_array.zarr", component="array_3D")
-
-zarr_out = z2.create_dataset("array_3D", data=array_3D, chunks=chunks_use, compressor=compressor)
-
 # %% END
